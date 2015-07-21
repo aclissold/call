d98ba29fea8f7449c675ed8f7dc1cef555611d52
Andrew Clissold <andrewclissold@gmail.com>
2015-07-20 20:01:45 -0400

    Initial commit

---
+++ call.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+/// Calls a shell command and returns its exit code.
+public func call(command: String) -> Int {
+    let task = NSTask()
+    task.launchPath = "/usr/bin/env"
+    task.arguments = split(command) { $0 == " " }
+
+    let pipe = NSPipe()
+    task.standardOutput = pipe
+    task.launch()
+
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    if let output = NSString(data: data, encoding: NSUTF8StringEncoding) as? String {
+        print(output)
+    }
+
+    return Int(task.terminationStatus)
+}
